37824a8d8e56bb4d52b2402a54275408a3901a2a
Swenwa <swenwa@gmail.com>
2022-01-10 13:43:06 +0100

    Added ressource automatisation for initial setup

---
+++ ressource-automatisation/initialisation.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#Create tenant_master table in DynamoDB for tenant resources
+#aws dynamodb create-table --table-name tenant_master --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+#Create User Pool for free subscription tenants
+USER_POOL_ID="$(aws cognito-idp create-user-pool --pool-name time-tracker-free --policies 'PasswordPolicy={MinimumLength=8,RequireUppercase=true,RequireLowercase=true,RequireNumbers=true,RequireSymbols=true,TemporaryPasswordValidityDays=7}' --username-attributes email --mfa-configuration OFF --username-configuration CaseSensitive=false --admin-create-user-config AllowAdminCreateUserOnly=false --schema Name=email,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=true,'StringAttributeConstraints={MinLength=1,MaxLength=256}' Name=first_name,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=1,MaxLength=256}' Name=last_name,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=1,MaxLength=256}' Name=sub_model,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=1,MaxLength=256}' Name=entrance_date,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=1,MaxLength=256}' Name=phone,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=0,MaxLength=256}' Name=avatar_url,AttributeDataType=String,DeveloperOnlyAttribute=false,Mutable=true,Required=false,'StringAttributeConstraints={MinLength=0,MaxLength=256}'| jq '.UserPool .Id')"
+USER_POOL_ID=${USER_POOL_ID#'"'}
+USER_POOL_ID=${USER_POOL_ID%'"'}
+#Create groups in User Pool
+aws cognito-idp create-group --group-name ROLE_USER --user-pool-id $USER_POOL_ID
+aws cognito-idp create-group --group-name ROLE_ADMIN --user-pool-id $USER_POOL_ID
+#Create Client in User Pool
+aws cognito-idp create-user-pool-client --user-pool-id $USER_POOL_ID --client-name app-client-free --generate-secret --refresh-token-validity 90 --access-token-validity 12 --id-token-validity 12 --explicit-auth-flows ALLOW_ADMIN_USER_PASSWORD_AUTH ALLOW_CUSTOM_AUTH ALLOW_USER_PASSWORD_AUTH ALLOW_USER_SRP_AUTH ALLOW_REFRESH_TOKEN_AUTH --prevent-user-existence-errors ENABLED | jq '.UserPoolClient' > UserPoolClient.json
+
+APP_CLIENT_ID=$(jq '.ClientId' UserPoolClient.json)
+APP_CLIENT_ID=${APP_CLIENT_ID#'"'}
+APP_CLIENT_ID=${APP_CLIENT_ID%'"'}
+
+APP_SECRET_ID=$(jq '.ClientSecret' UserPoolClient.json)
+APP_SECRET_ID=${APP_SECRET_ID#'"'}
+APP_SECRET_ID=${APP_SECRET_ID%'"'}
+
+item=$( jo id=$(jo S=time-tracker-free) appSecretId=$(jo S=$APP_SECRET_ID) appClientId=$(jo S=$APP_CLIENT_ID) userPoolId=$(jo S=$USER_POOL_ID)  | jq '.' > item.json)
+
+echo "User Pool Id:" $USER_POOL_ID
+echo "App Secret Id:" $APP_SECRET_ID
+echo  $APP_CLIENT_ID
+echo $item
+aws dynamodb put-item \
+    --table-name tenant_master \
+    --item file://item.json
